fc4d7a2bb04dd73d102192bd825cf63967278227
Venelin <venkoul99@gmail.com>
2024-07-31 23:25:44 +0300

    implement create game functionality

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,27 +1,91 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { useCreateGame } from "../../hooks/useGames";
+
+const initialValues = {
+  title: "",
+  category: "",
+  maxLevel: "",
+  imageUrl: "",
+  summary: "",
+};
+
 export default function GameCreate() {
-    return(
-        <section id="create-page" className="auth">
-        <form id="create">
-            <div className="container">
+  const navigate = useNavigate();
+  const createGame = useCreateGame();
 
-                <h1>Create Game</h1>
-                <label htmlFor="leg-title">Legendary title:</label>
-                <input type="text" id="title" name="title" placeholder="Enter game title..." />
+  const createHandler = async (values) => {
+    try {
+      const { _id: gameId } = await createGame(values);
 
-                <label htmlFor="category">Category:</label>
-                <input type="text" id="category" name="category" placeholder="Enter game category..." />
+      navigate(`/games/${gameId}`);
+    } catch (err) {
+      //TODO: set error state and display error
+      console.log(err.message);
+    }
+  };
 
-                <label htmlFor="levels">MaxLevel:</label>
-                <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
+  const { values, changeHandler, submitHandler } = useForm(
+    initialValues,
+    createHandler
+  );
 
-                <label htmlFor="game-img">Image:</label>
-                <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+  return (
+    <section id="create-page" className="auth">
+      <form id="create" onSubmit={submitHandler}>
+        <div className="container">
+          <h1>Create Game</h1>
+          <label htmlFor="leg-title">Legendary title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={values.title}
+            onChange={changeHandler}
+            placeholder="Enter game title..."
+          />
 
-                <label htmlFor="summary">Summary:</label>
-                <textarea name="summary" id="summary"></textarea>
-                <input className="btn submit" type="submit" value="Create Game" />
-            </div>
-        </form>
+          <label htmlFor="category">Category:</label>
+          <input
+            type="text"
+            id="category"
+            name="category"
+            value={values.category}
+            onChange={changeHandler}
+            placeholder="Enter game category..."
+          />
+
+          <label htmlFor="levels">MaxLevel:</label>
+          <input
+            type="number"
+            id="maxLevel"
+            name="maxLevel"
+            value={values.maxLevel}
+            onChange={changeHandler}
+            min="1"
+            placeholder="1"
+          />
+
+          <label htmlFor="game-img">Image:</label>
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            value={values.imageUrl}
+            onChange={changeHandler}
+            placeholder="Upload a photo..."
+          />
+
+          <label htmlFor="summary">Summary:</label>
+          <textarea
+            name="summary"
+            value={values.summary}
+            onChange={changeHandler}
+            id="summary"
+          ></textarea>
+          <input className="btn submit" type="submit" value="Create Game" />
+        </div>
+      </form>
     </section>
-    );
+  );
 }
